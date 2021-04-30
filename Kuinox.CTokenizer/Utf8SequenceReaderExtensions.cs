8a109d7d65f8cf06a0608cec81b50834ca41266c
Kuinox <nvandeginste@gmail.com>
2021-04-30 18:18:35 +0200

    Renamed package before publishing it.

--- Kuinox.CTokenizer/Utf8SequenceReaderExtensions.cs
+++ Kuinox.CTokenizer/Utf8SequenceReaderExtensions.cs
@@ -5,8 +5,6 @@ namespace Tokenizer
 {
     public static class Utf8SequenceReaderExtensions
     {
-        
-
         public static ReadOnlySequence<T> SliceToNow<T>( this SequenceReader<T> reader, SequencePosition startPosition ) where T : unmanaged, IEquatable<T>
             => reader.Sequence.Slice( startPosition, reader.Position );
     }
